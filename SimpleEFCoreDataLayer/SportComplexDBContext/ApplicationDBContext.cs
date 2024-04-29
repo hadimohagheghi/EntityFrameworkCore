97168ca822be60567af00ba8aef9e400f696d422
Hadi Mohagheghi <hadimohagheghi@live.com>
2024-04-29 15:36:03 +0330

    Add project files.

---
+++ SimpleEFCoreDataLayer/SportComplexDBContext/ApplicationDBContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SimpleEFCoreDataLayer.SportComplexDBContext
+{
+    public class ApplicationDBContext:DbContext
+    {
+        //log
+        //filter
+        //Add Entity
+        //Tracking
+        //...
+        public ApplicationDBContext()
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("ConnectionString");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options): base(options)
+        {
+            
+        }
+
+    }
+}
---
+++ SportComplexConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using SimpleEFCoreDataLayer.SportComplexDBContext;
+
+namespace SportComplexConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using(var context=new ApplicationDBContext()) 
+            {
+                
+            }
+        }
+    }
+}
