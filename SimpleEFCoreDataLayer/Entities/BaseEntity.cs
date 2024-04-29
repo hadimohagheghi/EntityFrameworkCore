dea757f0bad1482981dd17b2760cd83b24e52c0c
Hadi Mohagheghi <hadimohagheghi@live.com>
2024-04-29 17:01:56 +0330

    Create Class Person

--- SimpleEFCoreDataLayer/Entities/BaseEntity.cs
+++ SimpleEFCoreDataLayer/Entities/BaseEntity.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
+using SimpleEFCoreDataLayer.Enums;
 
 namespace SimpleEFCoreDataLayer.Entities
 {
@@ -10,6 +12,14 @@ namespace SimpleEFCoreDataLayer.Entities
     {
         public TKey Id { get; set; }
         public bool IsDeleted { get; set; }
-        
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Family { get; set; }
+        public string FatherName { get; set; }
+        public string NationalCode { get; set; }
+        public Gender Gender { get; set; }
     }
 }
